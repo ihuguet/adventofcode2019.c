c1b57e378348b61c6c67f9e5e4a45c5d9c300656
Íñigo Huguet <inigohuguet@hotmail.com>
2023-07-10 00:13:12 +0200

    Day 1: The Tyranny of the Rocket Equation

--- day01.c
+++ day01.c
@@ -2,38 +2,56 @@
 #include <stdlib.h>
 #include "aoc_input.h"
 
+static long
+calc_recursive(long val) {
+    val = val / 3 - 2;
+    if (val >= 0)
+        return val + calc_recursive(val);
+    else
+        return 0;
+}
+
 #ifndef TEST
 
-int main() {
-    GString *line;
+int
+main(int argc, char **argv) {
+    long total_part1 = 0;
+    long total_part2 = 0;
 
     AocInputReader *reader = aoc_input_reader_new("day01");
     if (reader == NULL)
-        return 1;
+        return EXIT_FAILURE;
 
+    GString *line;
     while ((line = aoc_input_reader_getline(reader)) != NULL) {
         long val = aoc_input_parse_num(line->str);
         if (val == PARSE_NUM_ERR)
-            return 2;
+            return EXIT_FAILURE;
 
-        printf("%ld\n", val);
+        total_part1 += val / 3 - 2;
+        total_part2 += calc_recursive(val);
     }
 
-    return 0;
+    printf("Part 1: total fuel = %ld\n", total_part1);
+    printf("Part 2: total fuel = %ld\n", total_part2);
+    return EXIT_SUCCESS;
 }
+
 
 #else
 
 void
-test1() {
-    g_assert(TRUE);
+test_recursive_calc() {
+    g_assert_cmpint(calc_recursive(14), ==, 2);
+    g_assert_cmpint(calc_recursive(1969), ==, 966);
+    g_assert_cmpint(calc_recursive(100756), ==, 50346);
 }
 
 int
 main(int argc, char **argv) {
     g_test_init(&argc, &argv, NULL);
 
-    g_test_add_func("/day01/test1", test1);
+    g_test_add_func("/day01/test_recursive_calc", test_recursive_calc);
 
     return g_test_run();
 }
